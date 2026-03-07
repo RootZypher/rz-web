5a522e63f694cce733b23258cf5f7241c072a2a8
RootZypher <rootzypher@gmail.com>
2026-03-07 19:51:44 +0530

    Ignore backend folder for public repo

--- backend/razorpay-config.js
+++
@@ -1,8 +0,0 @@
-const RAZORPAY_CONFIG = {
-    key_id: "rzp_test_SNuZaM0AeAFbn4",
-    key_secret: "FyyftO28Sm7d2GpVFMu751x7",
-    amounts: { trail: 5, main: 499 },
-    currency: "INR",
-    upi_handles: ["@okhdfcbank", "@ybl", "@paytm", "@okaxis"]
-};
-Object.freeze(RAZORPAY_CONFIG);
